3eeacf1ccbe9ed03d30043d6cb2dee918fafdccf
tian <tian@localhost.localdomain>
2018-06-13 16:07:52 +0800

    test4

---
+++ test.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <string.h>
+
+//enum sex
+//{
+//    MALE,
+//    FEMALE,
+//    SECRET
+//};
+//void test()
+//{
+//    static int num = 0;//对局部变量加static使其成为静态变量，改变其生命周期，直到程序结束
+//    //在第二次调用进入时可以保持第一次退出时的值
+//    num++;
+//    printf("%d\n",num);
+//}
+//void test1(int *arr)
+//{
+//    printf("test1 is %d\n",sizeof(arr));
+//}
+//void test2(char *ch)
+//{
+//    printf("test2 is %d\n",sizeof(ch));
+//}
+int main()
+{
+    //const int sz = 10;
+    //int arr[10];
+    //int arr2[sz];
+    //return 0;
+    //printf("%d\n",MALE);
+    //printf("%d\n",FEMALE);
+    //printf("%d\n",SECRET);
+   
+    
+    //
+    //int i = 0;
+    //int k = 0;
+    //int ret = 0;
+    //for(i = 0,k = -1;k = 0;i++,k++)
+    //{
+    //    k++;
+    //    ret++;
+    //}
+    //printf("%d\n",ret);
+   
+    
+    //char arr1[] = "welcome to bit...";
+    //char arr2[] = "#################";
+    //int left = 0;
+    //int right = strlen(arr1)-1;
+    //printf("%s\n",arr2);
+    //while(left <= right)
+    //{
+    //    sleep(1);
+    //    arr2[left] = arr1[left];
+    //    arr2[right] = arr1[right];
+    //    left++;
+    //    right--;
+    //    printf("%s\n",arr2);
+    //}
+   
+    
+    //int num1 = 1;
+    //int num2 = 2;
+    //printf("%d\n",num1 & num2);
+    //printf("%d\n",num1 | num2);
+    //printf("%d\n",num1 ^ num2);
+    //int arr[10] = {0};
+    //char ch[10] = {0};
+    //printf("arr is %d\n",sizeof(arr));
+    //printf("ch is %d\n",sizeof(ch));
+    //test1(arr);
+    //test2(ch);
+    //int i = 0,a = 0,b = 2,c = 3,d = 4;
+    //i = a++&&++b&&d++;
+    //printf("a = %d\n b = %d\n c = %d\n d = %d\n",a,b,c,d);
+    //int n = 10;
+    //char *pc = (char *)&n;
+    //int *pi = &n;
+    //printf("%p\n",&n);
+    //printf("%p\n",pc);
+    //printf("%p\n",pc+1);
+    //printf("%p\n",pi);
+    //printf("%p\n",pi+1);
+    //int arr[10] = {0};
+    //printf("%p\n",arr);
+    //printf("%p\n",arr+1);
+    //printf("%p\n",&arr+1);
+    //char str1[20];
+    //char str2[20];
+    //strcpy(str1,"to be ");
+    //strcpy(str2,"or not to be");
+    //strncat(str1,str2,6);
+    //puts(str1);
+    //struct s1
+    //{
+    //    int i;
+    //    char c;
+    //}un;
+    
+    
+    //union un1
+    //{
+    //    char c[5];
+    //    int i;
+    //}un1;
+    //union un2
+    //{
+    //    short c[7];
+    //    int i;
+    //}un2;
+    //printf("%d\n",sizeof(un1));
+    //printf("%d\n",sizeof(un2));
+    
+
+    
+    //union un1
+    //{
+    //    int a;
+    //    char c;
+    //}un1;
+    //un1.a = 1;
+    //if(un1.c == 1)
+    //{
+    //    printf("小端\n");
+    //}
+    //else
+    //{
+    //    printf("大端\n");
+    //}
+    //int a = 1;
+    //if((*(char *)&a) == 1)
+    //{
+    //    printf("xiaoduan\n");
+    //}
+    //else
+    //{
+    //    printf("大端\n");
+    //}
+    printf("hello makefile");
+    fflush(stdout);
+    sleep(3);
+    return 0;
+
+}
